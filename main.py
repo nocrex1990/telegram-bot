cb0c6c9cc7d16a3b8ed7066846de6549d98a2bbb
nocrex1990 <nocrex@gmail.com>
2025-05-28 11:14:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
-from flask import Flask, request
 
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "").strip()
+
 BASE_URL = "https://telegram-bot-rexx.onrender.com"
 WEBHOOK_PATH = "/webhook/" + TOKEN
 WEBHOOK_URL = BASE_URL + WEBHOOK_PATH
 
-app = Flask(__name__)
 application = ApplicationBuilder().token(TOKEN).build()
-
-@app.route(WEBHOOK_PATH, methods=["POST"])
-async def webhook():
-    print("👉 Richiesta ricevuta da Telegram!")
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    await application.process_update(update)
-    return "ok"
-
-@app.route("/", methods=["GET"])
-def index():
-    return "Bot Telegram attivo con webhook!"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot attivo con webhook su Render!")
@@ -35,4 +23,3 @@ def setup():
 
 if __name__ == "__main__":
     setup()
-
