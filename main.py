cd943c57452a724bd7b472de9fde11b90a116d4a
nocrex1990 <nocrex@gmail.com>
2025-05-30 22:11:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,6 +293,5 @@ async def run():
     await asyncio.Event().wait()
 
 # === MAIN ===
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
